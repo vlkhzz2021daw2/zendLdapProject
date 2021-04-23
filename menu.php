bdb059d2bb138df37608c45f9a4250c41bec3899
vlkh <vlkh@zend-vlkh.fjeclot.net>
2021-04-23 15:10:33 +0200

    Segon commit del projecte zendLdapProject

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
-		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
+		<a href="http://zend-vlkh.fjeclot.net/autent/visDades.php">Visualització de totes les dades</a>
 		<a href="http://zend-vlkh.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a>
 	</body>
 </html>
---
+++ visDades.php
@@ -0,0 +1,41 @@
+<?php
+    require 'vendor/autoload.php';
+    use Laminas\Ldap\Ldap;
+    
+    ini_set('display_errors',0);
+    if ($_GET['usr'] && $_GET['ou']){
+        
+        $domini = 'dc=fjeclot,dc=net';
+        $opcions = [
+            'host' => 'zend-vlkh.fjeclot.net',
+            'username' => "cn=admin,$domini",
+            'password' => 'fjeclot',
+            'bindRequiresDn' => true,
+            'accountDomainName' => 'fjeclot.net',
+            'baseDn' => 'dc=fjeclot,dc=net',
+        ];
+        $ldap = new Ldap($opcions);
+        $ldap->bind();
+        $entrada='uid='.$_GET['usr'].',ou='.$_GET['ou'].',dc=fjeclot,dc=net';
+        $usuari=$ldap->getEntry($entrada);
+        echo"<b><u>".$usuari["dn"]."</b></u><br>";
+        foreach ($usuari as $atribut => $dada) {
+            if ($atribut != "dn") echo$atribut.": ".$dada[0].'<br>';
+        }
+    }
+?>
+<html>
+    <head>
+        <title>
+        	MOSTRANT DADES D'USUARIS DE LA BASE DE DADES LDAP
+        </title>
+    </head>
+    <body>
+    	<form action="http://zend-vlkh.fjeclot.net/zendLdapProject/visDades.php" method="GET">
+        	Unitat organitzativa: <input type="text" name="ou"><br>
+        	Usuari: <input type="text" name="usr"><br>
+        	<input type="submit"/>
+        	<input type="reset"/>
+    	</form>
+    </body>
+</html>
